500df80cc89e19270eaa7073da9fcd5c97ec936a
Mahnoz Akhtari <akhtarim@icloud.com>
2025-10-05 12:53:40 -0400

    added header to bypass ngrok warning

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,14 +4,22 @@ import type { KOIItem, ModelName, PredictPayload, PredictResponse } from "./type
 const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000" || "https://noncalculable-unmurmurously-marybeth.ngrok-free.dev"
 
 export async function predict(payload: PredictPayload, model: ModelName = "rf") {
-  const response = await axios.post<PredictResponse>(`${API_BASE}/predict?model=${model}`, payload)
+  const response = await axios.post<PredictResponse>(`${API_BASE}/predict?model=${model}`, payload, {
+    headers: {
+      'ngrok-skip-browser-warning': true
+    }
+  })
   console.log("Response ", response)
   return response.data
 }
 
 export async function listKoi(): Promise<KOIItem[]> {
   try {
-    const { data } = await axios.get<KOIItem[]>(`${API_BASE}/koi/list`)
+    const { data } = await axios.get<KOIItem[]>(`${API_BASE}/koi/list`, {
+      headers: {
+        'ngrok-skip-browser-warning': true
+      }
+    })
     return data
   } catch {
     // Fallback to bundled sample if backend endpoint not present
